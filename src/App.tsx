873fe08184b63ffe3adac06a06f1a0768a1ef0d2
Joel Smith <joel.smith2226@gmail.com>
2023-07-06 09:30:04 +1000

    Deleted circles dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,53 @@
-import React, { useState } from 'react';
+import { Box, createTheme, ThemeProvider, Typography } from "@mui/material";
+import React from "react";
 import {
-  TextField,
-  Button,
-  Container,
-  Paper,
-  ThemeProvider,
-  createTheme,
-  Typography,
-  Grid,
-  IconButton,
-  Box,
-} from '@mui/material';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import PhotoCollage from './components/PhotoCollage.tsx';
-
-// Import the custom font
-import './fonts/Allan-Bold.ttf';
+  BrowserRouter as Router,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
+import "./App.css";
+import { Box1Page } from "./pages/Box1Page";
+import Root from "./routes/root";
+import { styled } from "@mui/material/styles";
 
 // Define the custom theme
 const theme = createTheme({
   palette: {
-    mode: 'dark', // Use dark mode
+    mode: "dark", // Use dark mode
     primary: {
-      main: '#9FBCFF', // Use light sand green as the primary color
+      main: "#9FBCFF", // Use light sand green as the primary color
     },
     background: {
-      default: '#111111', // Use a darker background color for the page
-      paper: '#222222', // Use a darker background color for the container
+      default: "#111111", // Use a darker background color for the page
+      paper: "#222222", // Use a darker background color for the container
     },
     // You can also define other colors such as secondary, error, warning, etc.
   },
   typography: {
-    fontFamily: 'Allan-Bold', // Use the custom font
+    fontFamily: "Allan-Bold", // Use the custom font
   },
 });
 
-const App: React.FC = () => {
-  const [answer, setAnswer] = useState('');
-  const [showPhotoCollage, setShowPhotoCollage] = useState(false);
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Box1Page />,
+  },
+]);
 
-  const handleAnswerChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAnswer(event.target.value);
-  };
-
-  const handleAnswerSubmit = () => {
-    // Check if answer is correct
-    if (answer === 'YourAnswerHere') {
-      setShowPhotoCollage(true);
-    } else {
-      // Show error message or perform other actions
-      console.log('Incorrect answer. Please try again.');
-    }
-  };
+export const App: React.FC<{}> = () => {
+  const Root = styled(Box)({
+    backgroundColor: "black",
+    height: "100%",
+    width: "100%",
+    position: "fixed",
+  });
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box
-        sx={{
-          width: '100vw',
-          height: '100vh',
-          backgroundColor: theme.palette.background.default, // Use default background color for the page
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Box
-          sx={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Container maxWidth="sm">
-            <Paper
-              elevation={3}
-              sx={{
-                p: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: theme.palette.background.paper, // Use paper background color for the container
-              }}
-            >
-              <Typography variant="h4" gutterBottom>
-                Mystery Box #1
-              </Typography>
-              <Typography variant="subtitle1" gutterBottom>
-                Please enter the correct answer to unlock the photo collage.
-              </Typography>
-              <TextField
-                label="Answer"
-                variant="outlined"
-                margin="normal"
-                value={answer}
-                onChange={handleAnswerChange}
-              />
-              <Button variant="contained" onClick={handleAnswerSubmit}>
-                Submit
-              </Button>
-              {showPhotoCollage && <PhotoCollage />}
-            </Paper>
-          </Container>
-        </Box>
-      </Box>
-    </ThemeProvider>
+    <Root>
+      <RouterProvider router={router} />
+    </Root>
   );
 };
-
-export default App;
--- src/components/PhotoCollage.tsx
+++ src/components/PhotoCollage.tsx
@@ -1,24 +1,10 @@
-// src/components/PhotoCollage.tsx
+import React from 'react';
 
-import React from "react";
+import { Box } from '@mui/material';
 
-interface PhotoCollageProps {
-  memory: string;
-  onNextButtonClick: () => void;
-}
-
-const PhotoCollage: React.FC<PhotoCollageProps> = ({ memory, onNextButtonClick }) => {
+export const PhotoCollage: React.FC<{}> = () => {
   return (
-    <div>
-      <h1>Memory Revealed</h1>
-      <p>{memory}</p>
-      <p>
-        {/* Render the photo collage here */}
-        {/* Add the Next button in the bottom right corner */}
-        <button onClick={onNextButtonClick}>Next</button>
-      </p>
-    </div>
+    <Box width="100%" display="flex" justifyContent="flex-end">
+    </Box>
   );
 };
-
-export default PhotoCollage;
